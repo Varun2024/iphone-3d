f21b08e1819d8b07ba8c42b53f7e510986b480b5
Varun2024 <marvellous079@gmail.com>
2026-01-03 16:16:13 +0530

    nav mod

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
 function Nav() {
-    return (  );
+    return (
+        // NAV: for semantic purposes of nav bar
+        <nav
+            className="nav-wrapper">
+                {/* for styling purposes */}
+                <div className="nav-content">
+                    <ul className='list-styled'>
+                        
+                    </ul>
+                </div>
+        </nav>
+    );
 }
 
 export default Nav;
