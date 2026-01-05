6de7aa1d9f1a787df00f07d3560b8abcff7af9ef
Varun2024 <marvellous079@gmail.com>
2026-01-05 23:41:00 +0530

    Jumbotron complete

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,8 +1,26 @@
-
-
-
+import React from 'react';
+import Iphone from '../assets/images/iphone-14.jpg';
+import HoldingIphone from '../assets/images/iphone-hand.png'
 function Jumbotron() {
-    return ( 'homlo' );
+    return (
+        <div className="jumbotron-section wrapper">
+            <h2 className='title'>New</h2>
+            <img src={Iphone} className='logo' alt="14pro" />
+            <p className='text'>Big and bigger</p>
+            <span className='description'>
+                From $41.62/mo. for 24 mo. or $999 before trade‑in.
+            </span>
+            <ul className='links'>
+                <li>
+                    <button className='button'>Buy</button>
+                </li>
+                <li>
+                    <a className='link'>Learn more</a>
+                </li>
+            </ul>
+            <img src={HoldingIphone} className='iphone-img' alt="holding iphone" />
+        </div>
+    );
 }
 
 export default Jumbotron;
---
+++ src/components/SoundSection.jsx
@@ -0,0 +1,15 @@
+function SoundSection() {
+    return (
+    <div className="sound-section wrapper">
+        <div className="body">
+            <div className="sound-section-content content">
+                <h2 className="title">New sound system.</h2>
+                <p className="text">Teel the base</p>
+                <span className="description">From $41.62/mo. for 24 mo. or $999 before trade‑in.</span>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default SoundSection;
